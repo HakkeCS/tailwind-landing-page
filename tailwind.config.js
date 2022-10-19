a4e91f4b83a1cb2470ec229cde0ba29d9fa995da
HakkeCS <104091187+HakkeCS@users.noreply.github.com>
2022-10-19 12:11:56 -0700

    started adding content and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     screens: {
       sm: '480px',
-      md: '768ps',
+      md: '768px',
       lg: '976px',
       xl: '1440px'
     },
@@ -12,7 +12,12 @@ module.exports = {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)'
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
       }
     },
   },
